a9487f2c75bd434e5218182fe3c6693fab63622d
PiyushTTN123 <piyush.gour@tothenew.com>
2022-11-24 12:53:47 +0530

    Saved all work

---
+++ myproject/core/src/main/java/com/myproject/core/models/BlogsListModel.java
@@ -0,0 +1,18 @@
+package com.myproject.core.models;
+
+import java.util.List;
+import javax.inject.Inject;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables=Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class BlogsListModel {
+
+	@Inject
+	private List<BlogsHomePage> blogList;
+
+	public List<BlogsHomePage> getBlogList() {
+		return blogList;
+	}
+}
